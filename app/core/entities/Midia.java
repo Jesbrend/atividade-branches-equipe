6bb28b43ea3967ff1708f449781b7f7ea20ef17c
lucasbringell <lucasbringeleite@gmail.com>
2025-11-08 15:59:33 -0300

    feat: adicionando todas as entidades + streaming app (main) da fase 1 do projeto

---
+++ app/core/entities/Midia.java
@@ -0,0 +1,29 @@
+package entities;
+
+import core.Genero;
+
+public abstract class Midia {
+    private String titulo;
+    private String artista;
+    private int duracaoEmSegundos;
+    private Genero genero;
+
+    public Midia(String titulo, String artista, int duracaoEmSegundos, Genero genero) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracaoEmSegundos = duracaoEmSegundos;
+        this.genero = genero;
+    }
+
+    public abstract String getTipo();
+
+    public String getTitulo() { return titulo; }
+    public String getArtista() { return artista; }
+    public int getDuracaoEmSegundos() { return duracaoEmSegundos; }
+    public Genero getGenero() { return genero; }
+
+    @Override
+    public String toString() {
+        return getTipo() + " [Título: " + titulo + ", Artista: " + artista + ", Duração: " + duracaoEmSegundos + "s, Gênero: " + genero + "]";
+    }
+}
