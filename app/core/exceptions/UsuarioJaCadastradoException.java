ff20d5b4803094fe8ebfe128eb16dd5a030bd38c
lucasbringell <lucasbringeleite@gmail.com>
2025-11-08 16:03:58 -0300

    feat: adicionando tratamento de exceções - adicionar duplicata na playlist, midia não encontrada e usuario já cadastrado

---
+++ app/core/exceptions/UsuarioJaCadastradoException.java
@@ -0,0 +1,7 @@
+package app.core.exceptions;
+
+public class UsuarioJaCadastradoException extends Exception {
+    public UsuarioJaCadastradoException(String message) {
+        super(message);
+    }
+}
